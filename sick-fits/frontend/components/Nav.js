57adda9b5f84a99cf38a5ea85e0eb2274114530c
leer <ladesigns90@gmail.com>
2020-05-29 16:18:06 -0400

    Applied global styling using Styled components. Fix styled components flicker on server render.

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,14 +1,25 @@
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 const Nav = () => (
-    <div>
-        <Link href="/">
-            <a>Home!</a>
+    <NavStyles>
+
+        <Link href="/items">
+            <a>Items</a>
         </Link>
         <Link href="/sell">
-            <a>Sell!</a>
+            <a>Sell</a>
+        </Link>
+        <Link href="/signups">
+            <a>Signup</a>
+        </Link>
+        <Link href="/orders">
+            <a>Orders</a>
+        </Link>
+        <Link href="/me">
+            <a>Account</a>
         </Link>
 
-    </div>
+    </NavStyles>
     
 )
 
